327d53de8e758a33332376cf5683442dbc1c64dc
Evans Pauliuts <pauliutsevans@gmail.com>
2019-11-13 17:37:38 +0300

    More on Conditionals

---
+++ more_on_conditionals.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# https://www.hackerrank.com/challenges/bash-tutorials---more-on-conditionals
+
+read X
+read Y
+read Z
+
+if [[ $X == $Y && $X == $Z ]]; then
+    echo 'EQUILATERAL'
+elif [[ $X == $Y || $X == $Z || $Y == $Z ]]; then
+    echo 'ISOSCELES'
+else
+    echo 'SCALENE'
+fi
